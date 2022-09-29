9de98f71592852b1322c70535eaf1cd4da19f0af
thelscon <thelscon@gmail.com>
2022-09-29 21:33:17 +0300

    continued...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import { ThemeService } from './services/theme.service';
 import { WindowService } from './services/window.service';
@@ -9,16 +10,49 @@ import { WindowService } from './services/window.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MLP';
 
   windowWidth = window.innerWidth ;
 
-  constructor ( 
+  visibleComponent !: boolean ;
+  playVisible !: boolean ;
+
+  constructor (
+    public title : Title ,
     public theme : ThemeService ,
     public windowService : WindowService ) {
+      
+      this.title.setTitle ( 'MLP' ) ;
+
+      this.visibleComponent = this.windowWidth >= 768 ? true : false ;
+      if ( this.visibleComponent ) {
+        setTimeout (
+          () => {
+            this.playVisible = true ;
+          }
+        )
+      }
+      else {
+        this.playVisible = false ;
+      }
+      
       this.windowService.resize.subscribe (
         ( event ) => {
-          this.windowWidth = window.innerWidth ;
+          if ( window.innerWidth >= 768 && this.visibleComponent === false ) {
+            this.visibleComponent = true ;
+            setTimeout (
+              () => {
+                this.playVisible = true ;
+              }
+            )
+          }
+          if ( window.innerWidth < 768 && this.visibleComponent ) {
+            this.playVisible = false ;
+            setTimeout (
+              () => {
+                this.visibleComponent = false ;
+              } , 200
+            )
+          }
         }
       )
     }
